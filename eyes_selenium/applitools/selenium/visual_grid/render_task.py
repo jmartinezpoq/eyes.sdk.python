054a7f6ec396af0f471dac88ff40da5a7bc731d1
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-02-21 19:39:08 +0200

    fix: raise an error if max retries have been reached in RenderTask

--- eyes_selenium/applitools/selenium/visual_grid/render_task.py
+++ eyes_selenium/applitools/selenium/visual_grid/render_task.py
@@ -77,6 +77,8 @@ class RenderTask(VGTask):
                 datetime_utils.sleep(
                     1500, msg="/render throws exception... sleeping for 1.5s"
                 )
+            if fetch_fails > self.MAX_FAILS_COUNT:
+                raise EyesError("Render is failed. Max count retries reached")
             if not render_requests:
                 continue
             need_more_dom = need_more_resources = False
@@ -235,6 +237,10 @@ class RenderTask(VGTask):
                 finally:
                     iterations += 1
                     datetime_utils.sleep(1500, msg="Rendering...")
+                if iterations > self.MAX_ITERATIONS:
+                    raise EyesError(
+                        "Max iterations in poll_render_status has been reached"
+                    )
                 if statuses or 0 < fails_count < 3:
                     break
             finished = bool(
